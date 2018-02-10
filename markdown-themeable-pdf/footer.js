5664e9de4bfcd9e45dc3e1a956408b933d7a2723
gbaman <gbaman@users.noreply.github.com>
2018-02-10 19:16:34 +0000

    Add NI Jam custom header/footer/style file

--- markdown-themeable-pdf/footer.js
+++ markdown-themeable-pdf/footer.js
@@ -4,10 +4,9 @@
  *
  * The default file can be found in the folder ~/.atom/packages/markdown-themeable-pdf/templates
  *
- * @param Object For example: {fileInfo: base: "Demo.md", dir: ".../markdown-themeable-pdf/tests", ext: ".md", name: "Demo"}, root: "/", exportType: "pdf", destFileBase: "Demo.pdf", destFile: "......./markdown-themeable-pdf/tests/Demo.pdf"}
- * @returns Object {{height: string, contents: string}}
+ * @returns {{height: string, contents: string}}
  */
-module.exports = function (info) {
+module.exports = function () {
     var dateFormat = function () {
         return (new Date()).toLocaleDateString('en-US', {
             weekday: 'long',
@@ -20,6 +19,6 @@ module.exports = function (info) {
     };
     return {
         height: '1cm',
-        contents: '<div style="float:left;">Page {{page}}/{{pages}}</div><div style="float:right;">&copy; Copyright ' + dateFormat() + ' by COMPANYNAME</div>'
+        contents: '<div style="float:left;">Page {{page}}/{{pages}}</div><div style="float:right;";  ' + '</div>'
     };
 };
--- markdown-themeable-pdf/header.js
+++ markdown-themeable-pdf/header.js
@@ -4,12 +4,11 @@
  *
  * The default file can be found in the folder ~/.atom/packages/markdown-themeable-pdf/templates
  *
- * @param Object For example: {fileInfo: base: "Demo.md", dir: ".../markdown-themeable-pdf/tests", ext: ".md", name: "Demo"}, root: "/", exportType: "pdf", destFileBase: "Demo.pdf", destFile: "......./markdown-themeable-pdf/tests/Demo.pdf"}
- * @returns Object {{height: string, contents: string}}
+ * @returns {{height: string, contents: string}}
  */
-module.exports = function (info) {
+module.exports = function () {
     return {
-        height: '2cm',
-        contents: '<div style="text-align: right;"><strong>' + info.fileInfo.base + '</strong> to <strong>' + info.destFileBase + '</strong> by <span style="color: #EC4634; font-size: 120%; text-transform: uppercase;">markdown-themeable-pdf</span></div>'
+        height: '0cm',
+        contents: ''
     };
 };
